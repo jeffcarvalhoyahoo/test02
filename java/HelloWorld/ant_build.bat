9faeee182bee506781394a87cea1ec7cee1bf9d3
jeff <jeffcarvalho@yahoo.com>
2017-05-12 17:15:09 -0400

    commit ant build files

---
+++ java/HelloWorld/ant_build.bat
@@ -0,0 +1,12 @@
+
+
+REM  set ANT_HOME=c:\data_software\Ant\apache-ant-1.10.1\
+
+set ANT_HOME=C:\Data\Ant\apache-ant-1.10.1
+set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_131\
+
+set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH
+
+REM ant clean
+REM ant jar
+REM ant run
