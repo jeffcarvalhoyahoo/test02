61fb3b0bbc1e6b4d7dcba229834022ae18dfacc3
jeffcarvalho <jeffcarvalho@yahoo.com>
2017-05-21 10:12:20 -0400

    Update build files

---
+++ java/HelloWorld/e_java.bat
@@ -0,0 +1,16 @@
+
+
+REM  set ANT_HOME=c:\data_software\Ant\apache-ant-1.10.1\
+
+set ANT_HOME=C:\Data\Ant\apache-ant-1.10.1
+set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_131\
+
+set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+
+
+REM uses manifest to specific he main class
+REM java -jar build\jar\HelloWorld.jar
+
+
+java -cp build\\classes  carvalho.HelloWorld
+
